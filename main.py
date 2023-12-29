99c0d959170491c007c1019a142f0a4c9f542003
Srujan <srujangowda66@gmail.com>
2023-12-29 13:05:42 +0530

    feat: Implement encapsulation in User and Bank classes
    - Added private attributes to User class with double underscores
    - Introduced public accessor methods for controlled access
    - Improved overall encapsulation and code readability

--- main.py
+++ main.py
@@ -1,32 +1,45 @@
 class User:
     def __init__(self):
-        self.name = input("Name: ")
-        self.age = int(input("Age: "))
-        self.gender = input("Gender: ")
-        self.initial_amount = int(input("Enter the amount you want to open bank account with: "))
+        # Private attributes with double underscores
+        self.__name = input("Name: ")
+        self.__age = int(input("Age: "))
+        self.__gender = input("Gender: ")
+        self.__initial_amount = int(input("Enter the amount you want to open bank account with: "))
 
     def get_user_info(self):
-        print(f'User name: {self.name}')
-        print(f'Age: {self.age}')
-        print(f'Gender: {self.gender}')
+        # Public method to access and display user information
+        print(f'User name: {self.__name}')
+        print(f'Age: {self.__age}')
+        print(f'Gender: {self.__gender}')
+
+    def get_user_name(self):
+        # Public method to access user's name
+        return self.__name
+
+    def get_initial_balance(self):
+        # Public method to access the initial balance
+        return self.__initial_amount
 
 
 class Bank(User):
     def __init__(self):
-        super().__init__()
-        self.balance = self.initial_amount
+        super().__init__()  # Call the constructor of the parent class (User)
+        self.balance = self.get_initial_balance()  # Initialize balance with the initial amount
 
     def view_balance(self):
-        print(f'Hi {self.name},'
+        # Public method to view the account balance
+        print(f'Hi {self.get_user_name()},'
               f'Your account balance is: {self.balance}')
 
     def deposit(self):
+        # Public method to deposit money into the account
         deposit_amount = int(input("Enter the amount you want to deposit: "))
         self.balance += deposit_amount
         print(f'Amount {deposit_amount} successfully deposited')
         print(f'Your account balance is {self.balance}')
 
     def withdraw(self):
+        # Public method to withdraw money from the account
         withdraw_amount = int(input("Enter the amount you want to withdraw: "))
 
         if self.balance >= withdraw_amount:
@@ -37,6 +50,7 @@ class Bank(User):
             print("Your balance is insufficient to withdraw")
 
     def transfer(self, target_account):
+        # Public method to transfer money to another account
         target_amount = int(input("Enter the amount to be transferred: "))
 
         if self.balance >= target_amount:
